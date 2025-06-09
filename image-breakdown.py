557df1a687734f370307ee5b461bb020388f621b
Nathan Balthazor <ntbal@NTBPC.>
2025-06-08 20:55:45 -0500

    More edits and accompanying Jupyter notebook file

--- image-breakdown.py
+++ image-breakdown.py
@@ -3,6 +3,9 @@
 # create 3x3x9 tensor of integers; 
 # build model to fill integers and correctly solve puzzle 
 from PIL import Image
+import cv2
+import pytesseract
+import numpy as np
 
 
 imgfile = Image.open("tmp_image.png")
@@ -36,11 +39,39 @@ def nine_split(imgfile):
     return crop_vec
 
 
+def digit_read(crop_vec):
+   #Iterating through list of images and creating tensor of digits 
+   unslvd_tensor = [[[0 for _ in range(9)] for _ in range(3)] for _ in range(3)]
+
+   for i,seg in enumerate(crop_vec):
+      #Converting image from PIL (png) to CV format
+      crop_cv = cv2.cvtColor(np.array(seg), cv2.COLOR_RGB2BGR)
+      #Preprocess image to gray scale, threshold it and invert it
+      gray = cv2.cvtColor(crop_cv, cv2.COLOR_BGR2GRAY)
+      thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+      #Run tesseract OCR to read the digits 
+      digit_config = r'--oem 3 --psm 10 -c tessedit_char_whitelist=0123456789'
+      text = pytesseract.image_to_string(thresh, config=digit_config).strip()
+
+      #Read and store results
+      if text.isdigit():
+         print(int(text))
+         print(i)
+      else:
+        print("failure to read\n")
+         
+      return crop_cv
+      
+
+
+
 crop_vec = nine_split(imgfile)
+crop_cv = digit_read(crop_vec)
 
 #for i,seg in enumerate(crop_vec):   #checking boxes are split correctly; may need to hit Ctrl+C afterward if your terminal freaks out
 #   seg.show()
-crop_vec[3].show()
+
 
 
 
